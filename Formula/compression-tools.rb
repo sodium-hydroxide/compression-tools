e47ae6d88eb91da051fa1708cd078c20ff2fdc76
Noah Blair <noahjblair@outlook.com>
2024-11-21 08:48:34 -0700

    Update SHA256 for v0.1.0

--- Formula/compression-tools.rb
+++ Formula/compression-tools.rb
@@ -1,15 +1,15 @@
 class CompressionTools < Formula
   desc "Collection of utilities for file compression operations"
   homepage "https://github.com/sodium-hydroxide/compression-tools"
-  url "https://github.com/yourusername/compression-utils/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "HASH_HERE" # You'll need to generate this after creating the release
+  url "https://github.com/sodium-hydroxide/compression-tools/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   def install
     # Install binaries and shared code
     bin.install Dir["bin/*"]
     prefix.install "src"
-    
+
     # Update the source path in the binary files
     inreplace Dir[bin/"*"] do |s|
       s.gsub! "../src", prefix/"src"
