a7907f472fe3d0376f8df462e87abbbc7b98ded6
Noah Blair <noahjblair@outlook.com>
2024-11-24 18:46:09 -0700

    Bump version to 0.1.3

--- Formula/compression-tools.rb
+++ Formula/compression-tools.rb
@@ -1,7 +1,7 @@
 class CompressionTools < Formula
   desc "Collection of utilities for file compression operations"
   homepage "https://github.com/sodium-hydroxide/compression-tools"
-  url "https://github.com/sodium-hydroxide/compression-tools/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/sodium-hydroxide/compression-tools/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "ef3e4b2089f17a54e80c84e809f731e42c782529c28fe4229a425caa122b5c81"
   license "MIT"
 
