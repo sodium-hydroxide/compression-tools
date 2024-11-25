859f1c98307abda431732d18cac62288ec446122
Noah Blair <noahjblair@outlook.com>
2024-11-24 18:46:13 -0700

    Update SHA256 for v0.1.3

--- Formula/compression-tools.rb
+++ Formula/compression-tools.rb
@@ -2,7 +2,7 @@ class CompressionTools < Formula
   desc "Collection of utilities for file compression operations"
   homepage "https://github.com/sodium-hydroxide/compression-tools"
   url "https://github.com/sodium-hydroxide/compression-tools/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "ef3e4b2089f17a54e80c84e809f731e42c782529c28fe4229a425caa122b5c81"
+  sha256 "968f9c96cfcc54029e1d35631cebd9bb7ba2a9e144973dc5a0abac8790e8f552"
   license "MIT"
 
   def install
